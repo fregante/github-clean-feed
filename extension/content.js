7ff031bf0f21ced1692f71cfacd238caa9d4d1c8
Federico Brigante <bfred-it@users.noreply.github.com>
2016-07-20 12:12:17 +0700

    Hide stargazers (optional)

--- extension/content.js
+++ extension/content.js
@@ -15,7 +15,7 @@ function usersHTML(users) {
 	.join(', ');
 }
 
-function groupRepos(selector, title, spawnPoint) {
+function groupRepos(selector, title, spawnPoint, options) {
 	const events = Array.from(document.querySelectorAll(selector));
 	const map = events.reduce((repos, item) => {
 		const user = item.querySelector('.title a:nth-child(1)').textContent;
@@ -36,15 +36,19 @@ function groupRepos(selector, title, spawnPoint) {
 		map.forEach((users, repoUrl) => {
 			const [owner, repo] = repoUrl.split('/');
 			listEl.appendChild(fromHTML(`
-				<li class="public source ">
+				<li class="public source ghgn-stargazers-${options.stargazers}">
 					<div class="mini-repo-list-item">
 						<a href="/${repoUrl}" class="css-truncate">
 							<svg aria-label="Repository" class="octicon octicon-repo repo-icon" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
 						<span class="repo-and-owner css-truncate-target">
 							<span class="owner css-truncate-target">${owner}</span>/<span class="repo">${repo}</span>
 						</span>
+						<span class="stars">
+							${users.size > 1 ? users.size : ''}
+							<svg aria-label="stars" class="octicon octicon-star" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14"><path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>
+						</span>
 					</a>
-					${usersHTML(users)}
+					<div class="ghgn-stargazers">${options.stargazers === 'none' ? '' : usersHTML(users)}</div>
 				</div>
 			</li>`));
 		});
@@ -55,8 +59,8 @@ function groupRepos(selector, title, spawnPoint) {
 
 function init(options) {
 	const spawnPoint = document.createElement('div');
-	groupRepos('.alert.watch_started', 'Starred repositories', spawnPoint);
-	groupRepos('.alert.fork', 'Forked repositories', spawnPoint);
+	groupRepos('.alert.watch_started', 'Starred repositories', spawnPoint, options);
+	groupRepos('.alert.fork', 'Forked repositories', spawnPoint, options);
 	if (options['use-sidebar']) {
 		const firstSideBox = document.querySelector('.dashboard-sidebar .boxed-group');
 		firstSideBox.parentNode.insertBefore(spawnPoint, firstSideBox);
