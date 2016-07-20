1352cf9060cb7972487bc2752d78556dc8a0404e
Federico Brigante <bfred-it@users.noreply.github.com>
2016-07-20 11:48:00 +0700

    Extended options support
    Radio and text elements

--- extension/options.js
+++ extension/options.js
@@ -1,20 +1,51 @@
-const inputs = Array.from(document.querySelectorAll('input'));
-inputs.forEach(el => {
-	const handleUpdate = e => {
-		const id = el.id;
-		chrome.storage.sync.set({
-			[id]: el.checked
-		});
-	};
-	el.addEventListener('input', handleUpdate);
-	el.addEventListener('change', handleUpdate);
-});
-function updateInputs() {
-	chrome.storage.sync.get(items => {
-		Object.keys(items).forEach(id => {
-			document.getElementById(id).checked = items[id];
-		});
+function handleInputUpdates(e) {
+	const el = e.target;
+	const name = el.name;
+	let value = el.value;
+	switch (el.type) {
+		case 'checkbox':
+			value = el.checked;
+			break;
+		default: break;
+	}
+	// console.info('Saving option', el.name, 'to', value);
+	chrome.storage.sync.set({
+		[name]: value
 	});
 }
-updateInputs();
-// chrome.storage.onChanged.addListener(updateInputs); //todo
+document.body.addEventListener('input', handleInputUpdates);
+document.body.addEventListener('change', handleInputUpdates);
+function updateInputs(items) {
+	Object.keys(items).forEach(name => {
+		const els = document.querySelectorAll(`[name="${name}"]`);
+		if (els.length) {
+			console.info('Set option', name, 'to', items[name]);
+			switch (els[0].type) {
+				case 'checkbox':
+					els[0].checked = items[name];
+					break;
+				case 'radio': {
+					const selected = Array.from(els)
+					.filter(el => el.value === items[name]);
+					if (selected.length) {
+						selected[0].checked = true;
+					}
+					break;
+				}
+				default:
+					els[0].value = items[name];
+					break;
+			}
+		} else {
+			console.warn('Stored option', name, items[name], 'was not found on the page', items[name]);
+		}
+	});
+}
+chrome.storage.sync.get(updateInputs);
+chrome.storage.onChanged.addListener(changes => {
+	const fields = {};
+	for (const name of Object.keys(changes)) {
+		fields[name] = changes[name].newValue;
+	}
+	updateInputs(fields);
+});
