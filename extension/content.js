fb9fadb82760c8b10c532a30887bb2922c9a014a
Federico Brigante <bfred-it@users.noreply.github.com>
2016-07-22 21:41:41 +0700

    Simplify element selector usage

--- extension/content.js
+++ extension/content.js
@@ -4,9 +4,18 @@ const iconStar = '<svg aria-label="Stars" class="octicon octicon-star" height="1
 const iconFork = '<svg aria-label="Fork" class="octicon octicon-git-branch dashboard-event-icon" height="16" role="img" version="1.1" viewBox="0 0 10 16" width="10"><path d="M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 0 0-1-3.72C.88 1 0 1.89 0 3a2 2 0 0 0 1 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>';
 const iconRepo = '<svg aria-label="Repository" class="octicon octicon-repo repo-icon" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>';
 
-function selectAll(selector) {
-	return Array.from(document.querySelectorAll(selector));
-}
+const $ = selector => document.querySelector(selector);
+const $$ = (selector, has) => {
+	let elements = Array.from(document.querySelectorAll(selector));
+	if (has) {
+		elements = elements.filter(el => el.querySelector(has));
+	}
+	elements.and = function () {
+		elements.splice(0, 0, ...$$(...arguments));
+		return elements;
+	};
+	return elements;
+};
 
 function fromHTML(html, all) {
 	const helper = document.createElement('div');
@@ -27,13 +36,12 @@ function groupRepos({action, elements, title, holder, actorsOnHover, icon = '', 
 	if (action === 'off') {
 		return;
 	}
-	const events = typeof elements === 'string' ? selectAll(elements) : elements;
 	if (action === 'hide') {
-		events.forEach(event => event.remove());
+		elements.forEach(elements => elements.remove());
 		return;
 	}
 
-	const map = events.reduce((repos, item) => {
+	const map = elements.reduce((repos, item) => {
 		const user = item.querySelector('.title a:nth-child(1)').textContent;
 		const repo = item.querySelector('.title a:nth-child(2)').textContent;
 		if (!repos.has(repo)) {
@@ -50,7 +58,7 @@ function groupRepos({action, elements, title, holder, actorsOnHover, icon = '', 
 	const listEl = fromHTML('<ul class="boxed-group-inner mini-repo-list"></ul>');
 
 	map.forEach((actors, repoUrl) => {
-		if (avoidDuplicates && document.querySelector(`.ghgn-group a[href="/${repoUrl}"]`)) {
+		if (avoidDuplicates && $(`.ghgn-group a[href="/${repoUrl}"]`)) {
 			return;
 		}
 		const [owner, repo] = repoUrl.split('/');
@@ -77,7 +85,7 @@ function groupRepos({action, elements, title, holder, actorsOnHover, icon = '', 
 		groupEl.appendChild(listEl);
 		holder.appendChild(groupEl);
 	}
-	events.forEach(event => event.remove());
+	elements.forEach(element => element.remove());
 }
 
 function apply(options) {
@@ -86,7 +94,7 @@ function apply(options) {
 
 	// handle stars
 	groupRepos({
-		elements: '.alert.watch_started',
+		elements: $$('.alert.watch_started'),
 		title: 'Starred repositories',
 		action: options.starredRepos,
 		icon: iconStar,
@@ -97,7 +105,7 @@ function apply(options) {
 
 	// handle forks
 	groupRepos({
-		elements: '.alert.fork',
+		elements: $$('.alert.fork'),
 		title: 'Forked repositories',
 		action: options.forkedRepos,
 		icon: iconFork,
@@ -106,11 +114,9 @@ function apply(options) {
 		holder,
 	});
 
-	// new/public repos (new branches are `.alert.create` too)
-	const newRepos = selectAll('.alert.create .octicon-repo').map(icon => icon.parentNode.parentNode.parentNode);
-	const publicRepos = selectAll('.alert.public');
+	// new/public repos
 	groupRepos({
-		elements: newRepos.concat(publicRepos),
+		elements: $$('.alert.create', '.octicon-repo').and('.alert.public'),
 		title: 'New repositories',
 		action: options.newRepos,
 		actorsOnHover,
@@ -119,46 +125,42 @@ function apply(options) {
 	});
 
 	// possibly hide new/deleted branches
-	const newBranches = selectAll('.alert.create .octicon-git-branch').map(icon => icon.parentNode.parentNode.parentNode);
-	const deletedBranches = selectAll('.alert.delete');
 	groupRepos({
-		elements: newBranches.concat(deletedBranches),
+		elements: $$('.alert.create', '.octicon-git-branch').and('.alert.delete'),
 		action: options.branches ? 'hide' : 'off',
 	});
 
 	// possibly hide tags and releases
-	const tags = selectAll('.alert.create .octicon-tag').map(icon => icon.parentNode.parentNode.parentNode);
-	const releases = selectAll('.alert.release');
 	groupRepos({
-		elements: tags.concat(releases),
+		elements: $$('.alert.create', '.octicon-tag').and('.alert.release'),
 		action: options.tags ? 'hide' : 'off',
 	});
 
 	// possibly hide pushed commits
 	groupRepos({
-		elements: '.alert.push',
+		elements: $$('.alert.push'),
 		action: options.commits ? 'hide' : 'off',
 	});
 
 	// possibly hide collaborator events
 	groupRepos({
-		elements: '.alert.member_add',
+		elements: $$('.alert.member_add'),
 		action: options.collaborators ? 'hide' : 'off',
 	});
 
 	if (holder.children.length) {
-		const newsFeed = document.querySelector('#dashboard .news');
+		const newsFeed = $('#dashboard .news');
 		if (options.insertionPoint) {
 			newsFeed.insertBefore(holder, options.insertionPoint);
 		} else {
-			const accountSwitcher = document.querySelector('.account-switcher');
+			const accountSwitcher = $('.account-switcher');
 			newsFeed.insertBefore(holder, newsFeed.children[accountSwitcher ? 1 : 0]);
 		}
 	}
 }
 
 function init(options) {
-	const newsFeed = document.querySelector('#dashboard .news');
+	const newsFeed = $('#dashboard .news');
 	apply(options);
 
 	// track future updates
@@ -172,7 +174,7 @@ function init(options) {
 
 	// try loading more
 	if (options.loadMore) {
-		const btn = document.querySelector('.ajax-pagination-btn');
+		const btn = $('.ajax-pagination-btn');
 		if (btn) {
 			btn.click();
 		}
