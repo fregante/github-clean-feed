f40e51fcf8926e41f6cf239efa2d6e23fd273f80
Federico Brigante <bfred-it@users.noreply.github.com>
2017-01-22 12:46:26 +0900

    XO fixes

--- extension/lib/object-like-map.js
+++ extension/lib/object-like-map.js
@@ -1,22 +1,23 @@
-{
-	this.ObjectMap = function (iterable) {
-		return new Proxy(new Map(iterable), {
-			has(target, property) {
-				return target.has(property);
-			},
-			set(target, property, value) {
-				return target.set(property, value);
-			},
-			get(target, property) {
-				switch (property) {
-					case 'size':
-						return target[property];
-					case 'forEach':
-						return target[property].bind(target);
-					default:
-						return target.get(property);
-				}
-			},
-		});
-	};
+function ObjectMap(iterable) {
+	return new Proxy(new Map(iterable), {
+		has(target, property) {
+			return target.has(property);
+		},
+		set(target, property, value) {
+			return target.set(property, value);
+		},
+		get(target, property) {
+			switch (property) {
+				case 'size':
+					return target[property];
+				case 'forEach':
+					return target[property].bind(target);
+				default:
+					return target.get(property);
+			}
+		},
+	});
+}
+if (typeof module === 'object') {
+	module.exports = ObjectMap;
 }
