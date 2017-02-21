1716052bb4de895299d0d26ff627dab3bd781378
Federico Brigante <bfred-it@users.noreply.github.com>
2017-02-21 18:48:46 +0700

    Move options form to HTML

--- src/options/index.js
+++ src/options/index.js
@@ -1,64 +1,6 @@
 import OptSync from 'webext-options-sync';
-import OptDom from '../../libs/optdom';
 
-const form = OptDom.join(
-	OptDom.title('Starred repositories'),
-	OptDom.radio('starredRepos', {
-		group: 'Group',
-		hide: 'Hide',
-		off: 'Do nothing',
-	}, {
-		className: 'one-line'
-	}),
-
-	OptDom.title('Forked repositories'),
-	OptDom.radio('forkedRepos', {
-		group: 'Group',
-		hide: 'Hide',
-		off: 'Do nothing',
-	}, {
-		className: 'one-line'
-	}),
-
-	OptDom.title('New and newly-public repositories'),
-	OptDom.radio('newRepos', {
-		group: 'Group',
-		hide: 'Hide',
-		off: 'Do nothing',
-	}, {
-		className: 'one-line'
-	}),
-
-	OptDom.title('New comments'),
-	OptDom.radio('comments', {
-		group: 'Group',
-		hide: 'Hide',
-		off: 'Do nothing',
-	}, {
-		className: 'one-line'
-	}),
-
-	OptDom.title('New issues and PRs'),
-	OptDom.radio('newIssues', {
-		group: 'Group',
-		hide: 'Hide',
-		off: 'Do nothing',
-	}, {
-		className: 'one-line'
-	}),
-
-	OptDom.title('Display'),
-	OptDom.checkbox('hideCollaborators', 'Hide <em>collaborator added</em> events'),
-	OptDom.checkbox('hideBranches', 'Hide <em>new</em> and <em>deleted</em> branches'),
-	OptDom.checkbox('hideTags', 'Hide <em>releases</em> and <em>tags</em>'),
-	OptDom.checkbox('hideCommits', 'Hide pushed commits'),
-	OptDom.checkbox('hideClosedIssues', 'Hide closed issues'),
-	''
-);
-
-document.querySelector('#options-form').appendChild(form);
-
-new OptSync().syncForm(form);
+new OptSync().syncForm(document.querySelector('#options-form'));
 
 // chrome.storage.onChanged.addListener(changes => {
 // 	const fields = {};
