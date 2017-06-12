bf8dc7f33f065e0c22e305732581cc7e2f3785ea
Federico Brigante <github@bfred.it>
2017-06-13 00:46:19 +0800

    Drop unrelated commented code

--- src/options/index.js
+++ src/options/index.js
@@ -2,15 +2,8 @@ import OptSync from 'webext-options-sync';
 
 new OptSync().syncForm(document.querySelector('#options-form'));
 
-// chrome.storage.onChanged.addListener(changes => {
-// 	const fields = {};
-// 	for (const name of Object.keys(changes)) {
-// 		fields[name] = changes[name].newValue;
-// 	}
-// 	updateInputs(fields);
-// });
-
 // Localization
 for (const el of document.querySelectorAll('[data-i18n]')) {
 	el.innerHTML = chrome.i18n.getMessage(el.dataset.i18n);
 }
+
